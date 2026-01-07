49d1e0dd1993bf63aebddf809ed096b6ac6e06a4
netayehoshafat <nettaye@gmail.com>
2026-01-07 21:31:56 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,31 +1,14 @@
-from PIL import Image
-import numpy as np
-from scipy.signal import convolve2d
-
-def load_image(path):
-    load_image = mpimg.imread('my_image.jpeg')
-    plt.imshow(load_image)
-    plt.imshow(load_image)
-    plt.axis('off')
-    plt.show()
-
-def edge_detection(image):
-    from scipy import signal
-    grayscale_image = np.mean(mpimg.imread('/content/my_image.jpeg'), axis=2)
-      kernelY = np.array([
-        [1, 0, -1],
-        [2, 0, -2],
-        [1, 0, -1]
-        ])
-      kernelX = np.array([
-        [-1, -2, -1],
-        [ 0,  0,  0],
-        [ 1,  2,  1]
-        ])
-      edgeY = signal.convolve2d(grayscale_image, kernelY, mode='same', boundary='fill', fillvalue=0)
-      edgeX = signal.convolve2d(grayscale_image, kernelX, mode='same', boundary='fill', fillvalue=0)
-      edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
-      return edgeMAG
-
-    edges = edge_detection('/content/my_image.jpeg')
-    plt.imshow(edges, cmap='gray')
+#lOAD_IMAGE
+def load_image(filename):
+  img = mpimg.imread(filename)
+  return img
+  
+#EDGE_DETECTION
+def edge_detection(image_array):
+  grayscale_image = np.mean(image_array, axis=2)
+  kernelY = np.array([[ 1,  2,  1], [ 0,  0,  0], [-1, -2, -1]])
+  kernelX = np.array([[-1, -2, -1], [ 0,  0,  0], [ 1,  2,  1]])
+  edgeY = convolve2d(grayscale_image, kernelY, mode='same', boundary='fill', fillvalue=0)
+  edgeX = convolve2d(grayscale_image, kernelX, mode='same', boundary='fill', fillvalue=0)
+  edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
+  return edgeMAG
