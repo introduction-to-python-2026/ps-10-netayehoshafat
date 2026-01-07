adc7ffafb8314a2849c86bcbef34ca33fc028c45
netayehoshafat <nettaye@gmail.com>
2026-01-07 21:30:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-
+#lOAD_IMAGE
+def load_image(filename):
+  img = mpimg.imread(filename)
+  return img
+  
+#EDGE_DETECTION
+def edge_detection(image_array):
+  grayscale_image = np.mean(image_array, axis=2)
+  kernelY = np.array([[ 1,  2,  1], [ 0,  0,  0], [-1, -2, -1]])
+  kernelX = np.array([[-1, -2, -1], [ 0,  0,  0], [ 1,  2,  1]])
+  edgeY = convolve2d(grayscale_image, kernelY, mode='same', boundary='fill', fillvalue=0)
+  edgeX = convolve2d(grayscale_image, kernelX, mode='same', boundary='fill', fillvalue=0)
+  edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
+  return edgeMAG
